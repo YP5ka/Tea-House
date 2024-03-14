8562eaa94cf3d6f97e3a1376638d11ead91c1e27
YP5ka <ypyka12@gmail.com>
2024-03-14 19:25:27 +0300

    Фикс блока с жестами

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import WebcamComponent from './webcamera';
 import React, { useState } from 'react';
 
-
 function App() {
-    const [theme,setTheme] = useState('light'); // состояние для смены темы
-    const switchTheme = () => {
-      setTheme((current) => (current === 'light' ? 'dark' : 'light'));
-    }; // переключатель темы
+  const [theme, setTheme] = useState('light'); // состояние для смены темы
+  const [stateArray, replaceContent] = useState('default'); // состояние  для свапа двух форм
+  const switchTheme = () => {
+    setTheme((current) => (current === 'light' ? 'dark' : 'light'));
+  }; // переключатель темы
 
-    const [stateArray, replaceContent] = useState('default'); // состояние  для свапа двух форм
-    const HandleClick = () => {
-        console.log('clicked');
-        replaceContent((current) => (current === 'default' ? 'reversed' : 'default'));
-    }; // свапер форм
+  const HandleClick = () => {
+    console.log('clicked');
+    replaceContent((current) => (current === 'default' ? 'reversed' : 'default'));
+  }; // свапер форм
 
   return (
     <div id={theme}>
       <div className="wrapper" id={stateArray}>
-        
         <header>
-            <h1 >Handshake!</h1>
-            <h3 className='pointer'>Profile</h3>
-            <h3 className='pointer'>Settings</h3>
-            <h3 className='pointer' onClick={switchTheme}>Dark mode</h3>
-        </header> 
-        
-        <main className="content " >
-        
+          <h1>Handshake!</h1>
+          <h3 className="pointer">Profile</h3>
+          <h3 className="pointer">Settings</h3>
+          <h3 className="pointer" onClick={switchTheme}>
+            Dark mode
+          </h3>
+        </header>
+
+        <main className="content ">
           <div className="block_txt">
-            <img src='./img/text.svg'/>
-            <textarea className="block_txt-textarea" placeholder="Начните вводить текст" name="textarea_1" id="Укажешь удобный" />
-            
+            <img src="./img/text.svg" />
+            <textarea
+              className="block_txt-textarea"
+              placeholder="Начните вводить текст"
+              name="textarea_1"
+              id="Укажешь удобный"
+            />
           </div>
 
-          <div className="replace" onClick={HandleClick} >
+          <div className="replace" onClick={HandleClick}>
             {theme === 'light' ? (
               <img src="/img/change-circle.svg" />
             ) : (
@@ -42,10 +45,15 @@ function App() {
           </div>
 
           <div className="block_gestures">
-            <div className='block_gestures-img'></div>
-            <img src='./img/hand.svg'/>
-            <p className='block_gestures-p'>Жесты будут показаны здесь</p>
-          </div >
+            <div className="block_gestures-img"></div>
+            <img src="./img/hand.svg" />
+            <textarea
+              className="block_gestures-textarea"
+              placeholder="Жесты будут показаны после ввода текста"
+              name="textarea_1"
+              id="Укажешь удобный"
+            />
+          </div>
 
           {/* <WebcamComponent/>  */}
         </main>
