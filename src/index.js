a81c533c6628d750ee30f479131b3ae080a05025
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-05-04 11:56:22 +0300

    editing file name

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import loginRegister from '.loginRegister';
 import './index.scss';
-import './loginRegiser.scss'
+import './loginRegister.scss'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/> 
-
+   {/*<App/>*/ } 
+   <loginRegister/>
   </React.StrictMode>,
 );
 
--- src/loginRegister.js
+++ src/loginRegister.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function loginRegiseter()
+function loginRegister()
 {
     
     return(
