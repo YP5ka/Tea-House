12b12ec8ae69076634ab69cd2d4ccb4344171d95
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-05-04 13:18:17 +0300

    register done x2

---
+++ src/Register.js
@@ -0,0 +1,27 @@
+import React from 'react';
+function Register(){
+    
+    return(
+        <div className='container'>
+            <body className='container_body'>
+                <div className='container_body_div_img'>
+                    <img className='container_body_img' src='/img/sign.svg'></img>
+                </div>
+                <form action='' className='container_form'>
+                    <input className='container_form_input' id='name' placeholder='Имя' type='text' required></input>
+                    <input className='container_form_input' id='surname' placeholder='Фамилия' type='text' required></input>
+                    <input className='container_form_input' id='email' placeholder='Эл. почта' type='email' required></input>
+                    <div className='container_form_div_btn'>
+                        <button className='container_form_btn' type='submit'>Регистрация</button>
+                    </div>
+                </form>
+                <div className='container_body_footer'>
+                    <p className='container_footer_p'>Уже есть аккаунт?</p>
+                    <a className='container_footer_a' href=''>Вход</a>
+                </div>
+                
+            </body>
+        </div>
+    );
+} 
+export default LoginRegister;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import LoginRegister from './LoginRegister';
+import Register from './Register';
 import './index.scss';
 import './logReg.scss';
 
@@ -9,7 +9,7 @@ import './logReg.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <LoginRegister/>
+   <Register/>
     {/*<App/>*/ } 
   </React.StrictMode>,
 );
