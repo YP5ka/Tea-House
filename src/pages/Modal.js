4afabc6818d1baa6241132fbb466ddb20e7f4021
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-05-06 00:18:12 +0300

    сделал модальное окно, сделал таймер сделал формы для ввода цифр с почты

--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import './Modal.scss'
+import '../Modal.scss'
+import './Register'
 
-const Modal = ({active, setActive}) =>{
+const Modal = ({active, setActive, children}) =>{
+
+    function clickHandler(){
+        setActive(false);
+        document.getElementById('name').value='';
+        document.getElementById('surname').value='';
+        document.getElementById('email').value='';
+
+        document.getElementById('fs').value='';
+        document.getElementById('sc').value='';
+        document.getElementById('tr').value='';
+        document.getElementById('fr').value='';
+    }
+
     return(
-        <div className = {active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-            <div className='modal__content' onClick={e => e.stopPropagation()}>
-
+        <div className = {active ? "modal active" : "modal"} onClick={clickHandler}>
+            <div className= {active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation()}>
+                {children}
             </div>
         </div>
     );
