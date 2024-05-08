2f74abc2eccaf771cbbef7e3473c77e23d5329ad
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-05-08 14:57:10 +0300

    убрал ссылку на login в eror page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -5,7 +5,6 @@ export default function NotFoundPage()
     return (
         <div className="Error_container">
             <p className="Error_container_p">404 error</p>
-            <Link className="Error_container_link" to='/'>Login</Link>
         </div>
     );
 };
