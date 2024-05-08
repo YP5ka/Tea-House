b5b95206471d73019023729c0196d850ea400d27
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-05-08 18:43:23 +0300

    переделал переключение цветов

--- src/pages/Main_App.js
+++ src/pages/Main_App.js
@@ -1,99 +1,15 @@
 import { queryByTestId } from '@testing-library/react';
+import { useState } from 'react';
 import React from 'react';
 
+
 function Main_App(){
-    function HandleClick_Main_App_leftColumn_gestureToText()
-    {
-        let temp = document.querySelector('.Main_App_leftColumn_gestureToText');
-        console.log(temp);
-        temp.classList.add('color');
-        temp = document.querySelector('#hand');
-        temp.src='/img/hand color.svg'
+    const [themeF, setColorF] = useState(0); // состояние для смены темы
+    const [themeS, setColorS] = useState(0); // состояние для смены темы
+    const [themeT, setColorT] = useState(0); // состояние для смены темы
+    const [themeFo, setColorFo] = useState(0); // состояние для смены темы
 
-        temp = document.querySelector('.Main_App_leftColumn_textToGesture');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_favorites');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_articles');
-        temp.classList.remove('color');
 
-        temp = document.querySelector('#A');
-        temp.src='/img/A normal.svg'
-        temp = document.querySelector('#like');
-        temp.src='/img/like normal.svg'
-        temp = document.querySelector('#news');
-        temp.src='/img/news normal.svg'
-        return;
-    }
-    function HandleClick_Main_App_leftColumn_textToGesture()
-    {
-        let temp = document.querySelector('.Main_App_leftColumn_textToGesture');
-        console.log(temp);
-        temp.classList.add('color');
-        temp = document.querySelector('#A');
-        temp.src='/img/A color.svg'
-
-        temp = document.querySelector('.Main_App_leftColumn_gestureToText');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_favorites');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_articles');
-        temp.classList.remove('color');
-
-        temp = document.querySelector('#hand');
-        temp.src='/img/hand normal.svg'
-        temp = document.querySelector('#like');
-        temp.src='/img/like normal.svg'
-        temp = document.querySelector('#news');
-        temp.src='/img/news normal.svg'
-        return;
-    }
-    function HandleClick_Main_App_leftColumn_favorites()
-    {
-        let temp = document.querySelector('.Main_App_leftColumn_favorites');
-        console.log(temp);
-        temp.classList.add('color');
-        temp = document.querySelector('#like');
-        temp.src='/img/like color.svg'
-        
-        temp = document.querySelector('.Main_App_leftColumn_textToGesture');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_gestureToText');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_articles');
-        temp.classList.remove('color');
-
-        temp = document.querySelector('#hand');
-        temp.src='/img/hand normal.svg'
-        temp = document.querySelector('#A');
-        temp.src='/img/A normal.svg'
-        temp = document.querySelector('#news');
-        temp.src='/img/news normal.svg'
-        return;
-    }
-    function HandleClick_Main_App_leftColumn_articles()
-    {  
-        let temp = document.querySelector('.Main_App_leftColumn_articles');
-        console.log(temp);
-        temp.classList.add('color');
-        temp = document.querySelector('#news');
-        temp.src='/img/news color.svg'
-
-        temp = document.querySelector('.Main_App_leftColumn_textToGesture');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_gestureToText');
-        temp.classList.remove('color');
-        temp = document.querySelector('.Main_App_leftColumn_favorites');
-        temp.classList.remove('color');
-
-        temp = document.querySelector('#hand');
-        temp.src='/img/hand normal.svg'
-        temp = document.querySelector('#A');
-        temp.src='/img/A normal.svg'
-        temp = document.querySelector('#like');
-        temp.src='/img/like normal.svg'
-        return ;
-    }
     return (
         <div className='Main_App_container'>
             <div className='Main_App_leftColumn'>
@@ -102,27 +18,68 @@ function Main_App(){
                     <p className='Main_App_leftColumn_user_p'>ФИО пользователя</p>
                 </div>
                 <div className='Main_App_leftColumn_div'>
-                    <div className='Main_App_leftColumn_gestureToText flex' onClick={HandleClick_Main_App_leftColumn_gestureToText}>
-                        <img id='hand' src='/img/hand normal.svg' className='Main_App_leftColumn_block_img'></img>
-
-                        <p className='Main_App_leftColumn_p'>жесты→текст</p>
+                    <div className='Main_App_leftColumn_gestureToText flex' onClick={() => {setColorF(1);  setColorS(0); setColorT(0); setColorFo(0);}}>
+                        {themeF === 0 ? ( <>
+                            <img id='hand' src='/img/hand normal.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p'>жесты→текст</p>
+                        </>
+                        ) : ( <>
+                            <img id='hand' src='/img/hand color.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p color'>жесты→текст</p>
+                            </>       
+                        )}
                     </div>
-                    <div className='Main_App_leftColumn_textToGesture flex' onClick={HandleClick_Main_App_leftColumn_textToGesture}> 
-                        <img id='A' src='/img/A normal.svg' className='Main_App_leftColumn_block_img'></img>
-                        <p className='Main_App_leftColumn_p'>текст→жесты</p>
+                    <div className='Main_App_leftColumn_textToGesture flex' onClick={() => {setColorF(0);  setColorS(1); setColorT(0); setColorFo(0);}}> 
+                        {themeS === 0 ? ( <>
+                            <img id='A' src='/img/A normal.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p'>текст→жесты</p>
+                            </>
+                        ) : ( <>
+                            <img id='A' src='/img/A color.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p color'>текст→жесты</p>
+                            </>       
+                        )}
                     </div>
-                    <div className='Main_App_leftColumn_favorites flex' onClick={HandleClick_Main_App_leftColumn_favorites}>
-                        <img id='like' src='/img/like normal.svg' className='Main_App_leftColumn_block_img'></img>
-                        <p className='Main_App_leftColumn_p'>избранное</p>
+                    <div className='Main_App_leftColumn_favorites flex' onClick={() => {setColorF(0);  setColorS(0); setColorT(1); setColorFo(0);}}>
+                        {themeT === 0 ? ( <>
+                            <img id='like' src='/img/like normal.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p'>избранное</p>
+                            </>
+                        ) : ( <>
+                            <img id='like' src='/img/like color.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p color'>избранное</p>
+                            </>       
+                        )}
                     </div>
-                    <div className='Main_App_leftColumn_articles flex' onClick={HandleClick_Main_App_leftColumn_articles}>
-                        <img id='news' src='/img/news normal.svg' className='Main_App_leftColumn_block_img'></img>
-                        <p className='Main_App_leftColumn_p'>статьи</p>
+                    <div className='Main_App_leftColumn_articles flex' onClick={() => {setColorF(0);  setColorS(0); setColorT(0); setColorFo(1);}}>
+                        {themeFo === 0 ? ( <>
+                            <img id='news' src='/img/news normal.svg' className='Main_App_leftColumn_block_img'></img>
+                            <p className='Main_App_leftColumn_p'>статьи</p>
+                            </>
+                        ) : ( <>
+                             <img id='news' src='/img/news color.svg' className='Main_App_leftColumn_block_img'></img>
+                             <p className='Main_App_leftColumn_p color'>статьи</p>
+                            </>       
+                        )}
                     </div>
                 </div>
             </div>
-            <div className='Main_App_rightColumn'>
+            <div className='Main_App_rightColumn' id='rightColumn'>
                 <img className='Main_App_rightColumn_img' src='/img/sign.svg'/>
+                <div id='lF' className='Main_App_rightColumn_block_gestureToText disable'>
+                    
+                </div>
+                <div id='lS' className='Main_App_rightColumn_block_textToGesture disable'>
+                    <form className='Main_App_rightColumn_block_textToGesture_form'>
+                        <textarea className='' placeholder='введите текст' /> 
+                    </form>
+                </div>
+                <div id='lT' className='Main_App_rightColumn_block_favorites disable'>
+                    
+                </div>
+                <div id='lF' className='Main_App_rightColumn_block_articles disable'>
+
+                </div>
             </div>
         </div>
         
